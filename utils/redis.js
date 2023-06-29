56af2a9decebd72669f4580f8f0134d8375eef9a
Mohamed Suliman <sulimmoh@gmail.com>
2023-06-28 20:18:11 -0400

    redis client

---
+++ utils/redis.js
@@ -0,0 +1,32 @@
+import redis from 'redis';
+import util from 'util';
+
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient();
+    this.client.on('error', (error) => {
+      console.log(`Redis client not connected to the server: ${error.message}`);
+    });
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    const getAsync = util.promisify(this.client.get).bind(this.client);
+    const reply = await getAsync(key);
+    return reply;
+  }
+
+  async set(key, value, duration) {
+    this.client.set(key, value, 'EX', duration);
+  }
+
+  async del(key) {
+    this.client.del(key);
+  }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;

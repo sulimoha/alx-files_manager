2fdd9676922e44989853321d5178af4eb6282150
Mohamed Suliman <sulimmoh@gmail.com>
2023-06-28 21:10:57 -0400

    add MongoDB utils

---
+++ utils/db.js
@@ -0,0 +1,29 @@
+import mongodb from 'mongodb';
+
+class DBClient {
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
+    const url = `mongodb://${host}:${port}`;
+
+    mongodb.MongoClient.connect(url, { useUnifiedTopology: true }, (error, client) => {
+      this.db = client.db(database);
+    });
+  }
+
+  isAlive() {
+    return !!this.db;
+  }
+
+  async nbUsers() {
+    return this.db.collection('users').countDocuments();
+  }
+
+  async nbFiles() {
+    return this.db.collection('files').countDocuments();
+  }
+}
+
+const dbClient = new DBClient();
+export default dbClient;
